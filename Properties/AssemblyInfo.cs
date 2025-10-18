a2a1128fdb95d37b95e2bd73b8cfc3f08092c27f
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2025-10-18 11:20:43 +0200

    add project file

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChaosFramework.Graphics.OpenGL")]
+[assembly: AssemblyProduct("ChaosFramework.Graphics.OpenGL")]
+[assembly: AssemblyCompany("ChaosTechnology")]
+[assembly: AssemblyCopyright("Copyright © ChaosTechnology 2025")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
